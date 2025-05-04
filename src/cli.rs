b6b59a73a71b490d919b89b1a85b1bc6de81f257
muditDhiway <mudit.sarda@dhiway.com>
2025-05-04 17:49:43 +0530

    remove secret

--- src/cli.rs
+++ src/cli.rs
@@ -10,7 +10,7 @@ use std::path::PathBuf;
 /// cargo run -- --path ./iroh_data
 
 /// # Run with persistent path and custom secret key
-/// cargo run -- --path ./iroh_data --secret-key c6135803322e8c268313574920853c7f940489a74bee4d7e2566b773386283f2
+/// cargo run -- --path <path> --secret-key <your_secret_key>
 /// ```
 #[derive(Parser, Debug)]
 #[command(name = "Starter Kit")]
